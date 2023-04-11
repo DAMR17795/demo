86b2c20791f188988c09c3db284c9a4abf9ee6aa
Daniel <dmarrom577@g.educaand.es>
2023-04-11 10:53:20 +0200

    segunda subida

--- src/test/java/demo/AppTest.java
+++ src/test/java/demo/AppTest.java
@@ -1,5 +1,6 @@
 package demo;
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
 import org.junit.Test;
@@ -17,4 +18,19 @@ public class AppTest
     {
         assertTrue( true );
     }
+
+    public int suma(int numA, int numB) {
+        return numA+numB;
+    }
+
+    @Test
+    public void testSuma() {
+        AppTest calc = new AppTest();
+        int resultado = calc.suma(2, 3);
+        assertEquals(5, resultado);
+    }
+
+
+
+
 }
